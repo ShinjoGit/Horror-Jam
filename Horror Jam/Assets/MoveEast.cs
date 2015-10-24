f98e1bc31d2dc2652e4c9da550e51615d388b399
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-10-24 02:26:27 -0400

    Most of the tile system is working.

---
+++ Horror Jam/Assets/MoveEast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEast : MonoBehaviour 
+{
+    public GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<ManageTiles>().UpdateEast();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (player.GetComponent<ManageTiles>().EWTrigger)
+            player.GetComponent<ManageTiles>().EWTrigger = false;
+    }
+}
---
+++ Horror Jam/Assets/MoveWest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWest : MonoBehaviour 
+{
+    public GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<ManageTiles>().UpdateWest();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (player.GetComponent<ManageTiles>().EWTrigger)
+            player.GetComponent<ManageTiles>().EWTrigger = false;
+    }
+}
