85582e2136496f6a0ad11d2f4c5fe2a6a1e0f9cc
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-10-24 19:04:30 -0400

    Credits and Game over
    +Added Credits scene
    +Added Game Over scene

---
+++ Horror Jam/Assets/GameOver_Manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver_Manager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void buttonPress(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+}
