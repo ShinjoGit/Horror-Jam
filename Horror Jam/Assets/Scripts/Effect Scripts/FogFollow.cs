aad557e7db8f8acd13597c0f241eb74a9490c5be
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-10-24 10:37:25 -0400

    Added Skybox to game

---
+++ Horror Jam/Assets/Scripts/Effect Scripts/FogFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogFollow : MonoBehaviour 
+{
+    // follow the position of player
+    public GameObject player;
+
+	void Awake () 
+    {
+	    if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+	}
+	
+	void Update () 
+    {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 100.0f, player.transform.position.z);
+	}
+}
